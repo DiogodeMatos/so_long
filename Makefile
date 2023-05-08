f30cac058d3603095786e6139e24aaf608afcfbb
Diogo Andre Carrilho De Matos <mtd5000@gmail.com>
2023-05-08 16:08:12 +0100

    makefile

---
+++ Makefile
@@ -0,0 +1,35 @@
+NAME = so_long
+
+SRC = so_long.c
+	
+CC = cc -g
+RM = rm -rf
+CFLAGS = -Wall -Wextra -Werror
+MLXFLAGS = -L ./minilibx-linux -lmlx -lX11 -lXext -lm
+
+OBJ = $(SRC:%.c=%.o)
+BONUS = $(BNS:%.c=%.o)
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+		$(MAKE) -C ./libft
+		$(MAKE) -C ./minilibx-linux
+		@$(CC) $(CFLAGS) $(OBJ) ./libft/libft.a $(MLXFLAGS) -o $(NAME)
+		
+clean:
+		$(MAKE) clean -C ./libft
+		$(MAKE) clean -C ./minilibx-linux
+		@$(RM) $(OBJ)
+		
+fclean: clean
+		$(MAKE) fclean -C ./libft
+		@$(RM) $(NAME) 
+		
+re: fclean all
+		$(MAKE) re -C ./libft
+		$(MAKE) re -C ./minilibx-linux
+
+so:
+	$(CC) -nostartfiles -fPIC $(CFLAGS) $(SRC)
+	cc -nostartfiles -shared -o libft.so $(OBJ)
---
+++ ft_checkmaperror.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_checkmaperror.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcarrilh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/03 11:29:25 by dcarrilh          #+#    #+#             */
+/*   Updated: 2023/05/03 11:29:27 by dcarrilh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void ft_checkber(char **argv)
+{
+	static int	a;
+
+	while (argv[1][a])
+	{
+		
+	}
+}
+
+void	ft_checkmaperror(char **argv)
+{
+	ft_checkber(argv);
+}
